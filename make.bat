113589bb723cb11ce53f2c09e3c87cf1c331b60c
NeverDecaf <jtr9289@gmail.com>
2017-01-10 08:32:42 -0500

    Added a few comments to make it easier to reproduce the specfile.

--- make.bat
+++ make.bat
@@ -1,2 +1,4 @@
 REM add Tree(r'!MMCE_Win32', prefix=r'!MMCE_Win32') + [(r'book.ico',r'book.ico','DATA')], as arg to EXE()
+REM change name to MT.exe
+REM pyi-makespec --onefile --noconsole --icon book.ico qtable.py
 pyinstaller --clean --noconfirm MT.spec
